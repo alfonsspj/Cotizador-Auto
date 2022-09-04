ee64207cbee157c6230edcf4876d63a28ac65c66
alfons <alfonsspj@gmail.com>
2022-09-03 22:58:41 -0300

    Añadiendo algoritmo de Cotizacion

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,7 +5,7 @@ import Error from './Error'
 
 const Formulario = () => {
 
-    const { datos, handleChangeDatos, error, setError } = useCotizador();
+    const { datos, handleChangeDatos, error, setError, cotizarSeguro } = useCotizador();
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -19,6 +19,7 @@ const Formulario = () => {
         setError('')
 
         // TODO : Cotizar
+        cotizarSeguro()
     }
     
   return (
--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,4 +1,5 @@
 import { useState, createContext } from 'react';
+import { obtenerDiferenciaYear, calcularMarca, calcularPlan, formatearDinero } from '../helpers'
 
 const CotizadorContext = createContext()
 
@@ -11,7 +12,9 @@ const CotizadorProvider = ({children}) => {
         plan: ''
     })
 
-    const [error, setError] = useState('')
+    const [error, setError] = useState('')    
+    const [resultado, setResultado] = useState(0)
+    const [cargando, setCargando] = useState(false)
 
     const handleChangeDatos = e => {
         setDatos({
@@ -20,14 +23,50 @@ const CotizadorProvider = ({children}) => {
         })
     }
 
+    const cotizarSeguro = () => {
+        // Una base 
+        let resultado = 2000
+
+        // Obtener diferencia de años
+        const diferencia = obtenerDiferenciaYear(datos.year)
+
+        // Hay que restar el 3% por cada año
+        resultado -= ((diferencia * 3) * resultado) / 100
+
+        // Europeo 30%
+        // Americano 15%
+        // Asiatico 5%
+        resultado *= calcularMarca(datos.marca)
+
+        // Básico 20%
+        // Completo 50%
+        resultado *= calcularPlan(datos.plan)
+        // resultado = resultado.toFixed(2) // devolver con 2 decimales
+
+        // Formatear Dinero
+        resultado = formatearDinero(resultado)
+        // console.log(resultado);
+
+        setCargando(true)
+
+        // spinner
+        setTimeout(() => {
+            setResultado(resultado)
+            setCargando(false)
+        }, 3000);
+    }
+
+
     return(
         <CotizadorContext.Provider
             value={{
                 datos,
                 handleChangeDatos,
                 error,
-                setError
-
+                setError,
+                cotizarSeguro,
+                resultado,
+                cargando
             }}
         >
             {children}
